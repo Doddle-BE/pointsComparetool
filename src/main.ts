b99e802ac563edc84c611a03e3e562e745251bd3
Joris Heyens <joris.heyens@doddle.be>
2023-07-18 10:04:58 +0200

    feat: filter and save file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import type { GeoJSON } from "geojson";
 import geoJson from "./geojson.json";
 import "./style.css";
 import * as turf from '@turf/turf';
@@ -18,6 +19,21 @@ const writeToLocalStorage = (id: string, value: string) => {
   const geojsonObject = JSON.parse(localStorage.getItem("geojsonObject") || "{}");
   geojsonObject[id] = value;
   localStorage.setItem("geojsonObject", JSON.stringify(geojsonObject));
+};
+const readFromLocalStorage = (goodOrFault = "all") => {
+  const geojsonObject = JSON.parse(localStorage.getItem("geojsonObject") || "{}");
+  const keys = Object.keys(geojsonObject);
+
+  if (goodOrFault === "good") {
+    return keys.filter((key) => geojsonObject[key] === "good");
+  }
+  if (goodOrFault === "fault") {
+    return keys.filter((key) => geojsonObject[key] === "fault");
+  }
+  return geojsonObject;
+};
+const filterGeojson = (filterFn: (feature) => boolean) => {
+  return geoJson.features.filter(filterFn);
 };
 
 // MAP code
@@ -74,9 +90,30 @@ map.on("load", () => {
 });
 
 
-//SIDEBAR code
+// SIDEBAR code
 const sidebar = document.getElementById("sidebar");
 
+// SIDEBAR - SAVE FILE BUTTON
+const saveFileButton = document.createElement("button");
+saveFileButton.id = "btnSaveFile";
+saveFileButton.innerText = "💾";
+saveFileButton.addEventListener("click", () => {
+  const geojsonObject = localStorage.getItem("geojsonObject") || "{}";
+
+  const blob = new Blob([geojsonObject], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const downloadLink = document.createElement('a');
+  downloadLink.href = url;
+  downloadLink.download = 'geocoderGoedFout.json';
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+});
+
+sidebar?.appendChild(saveFileButton);
+
+// SIDEBAR - FILTER
 const featuresDiv = document.createElement("div");
 featuresDiv.className = "featuresDiv";
 featuresDiv.innerHTML = `
@@ -84,18 +121,18 @@ featuresDiv.innerHTML = `
   <legend>Selecteer op map</legend>
   <div class="filter-options">
     <div>
-      <input type="radio" id="huey" name="drone" value="huey" />
-      <label for="huey">Allemaal</label>
+      <input type="radio" id="rbAll" name="rbFilter" value="all" checked />
+      <label for="rbAll">Allemaal</label>
     </div>
 
     <div>
-      <input type="radio" id="dewey" name="drone" value="dewey" />
-      <label for="dewey">Correct</label>
+      <input type="radio" id="rbGood" name="rbFilter" value="good" />
+      <label for="rbGood">Correct</label>
     </div>
 
     <div>
-      <input type="radio" id="louie" name="drone" value="louie" />
-      <label for="louie">Fout</label>
+      <input type="radio" id="rbFault" name="rbFilter" value="fault" />
+      <label for="rbFault">Fout</label>
     </div>
   </div>
 </fieldset>
@@ -103,60 +140,104 @@ featuresDiv.innerHTML = `
 
 sidebar?.appendChild(featuresDiv);
 
-for (let i = 0; i < geoJson.features.length; i += 2) {
-  const feature = geoJson.features[i];
+document.querySelectorAll("input[type=radio]").forEach((radio) => {
+  radio.addEventListener("change", (e) => {
+    items.innerHTML = "";
+    const value = (e.target as HTMLInputElement).value;
 
-  const featureDiv = document.createElement("div");
-  featureDiv.dataset.id = feature.properties.id;
-  featureDiv.innerHTML = `
-    <div>
-      <h4>${feature.properties.street} ${feature.properties.houseNumber ? feature.properties.houseNumber : ""}</h4>
-      <h4>${feature.properties.postalCode ? feature.properties.postalCode : ""} ${feature.properties.community ? feature.properties.community : ""}</h4>
-    </div>
-    <div id="goodOrFault">
-      <button id="good">✅</button>
-      <button id="fault">❌</button>
-    </div>
-    `;
-  featureDiv.className = "featureDiv";
+    if (value === "all") {
+      const filtered = filterGeojson(f => true);
 
-  featureDiv.addEventListener("click", () => {
-    map.setFilter("markers", ["==", "id", feature.properties.id]);
-    const filtered = geoJson.features.filter(f => f.properties.id === feature.properties.id);
-    const bboxCoords = turf.bbox({
-      type: "FeatureCollection",
-      features: filtered
+      renderItemsList(filtered);
+      map.setFilter("markers", null);
+    }
+    if (value === "good") {
+      const filtered = filterGeojson(f => [...readFromLocalStorage("good")].includes(f.properties.id));
+
+      renderItemsList(filtered);
+      map.setFilter("markers", ["in", "id", ...filtered.map(f => f.properties.id)]);
+    }
+    if (value === "fault") {
+      const filtered = filterGeojson(f => [...readFromLocalStorage("fault")].includes(f.properties.id));
+
+      renderItemsList(filtered);
+      map.setFilter("markers", ["in", "id", ...filtered.map(f => f.properties.id)]);
+    }
+  });
+
+});
+
+// SIDEBAR - ITEMS
+const items = document.createElement("div");
+items.className = "features";
+sidebar?.appendChild(items);
+
+const renderItemsList = (filtered) => {
+  for (let i = 0; i < filtered.length; i += 1) {
+    const feature = filtered[i];
+
+    const featureDiv = document.createElement("div");
+    featureDiv.dataset.id = feature.properties.id;
+    featureDiv.innerHTML = `
+      <div>
+        <h4>${feature.properties.street} ${feature.properties.houseNumber ? feature.properties.houseNumber : ""}</h4>
+        <h4>${feature.properties.postalCode ? feature.properties.postalCode : ""} ${feature.properties.community ? feature.properties.community : ""}</h4>
+      </div>
+      <div id="goodOrFault">
+        <button id="good">✅</button>
+        <button id="fault">❌</button>
+      </div>
+      `;
+    featureDiv.className = "featureDiv";
+
+    featureDiv.addEventListener("click", () => {
+      map.setFilter("markers", ["==", "id", feature.properties.id]);
+      const filtered = filterGeojson(f => f.properties.id === feature.properties.id)
+      const bboxCoords = turf.bbox({
+        type: "FeatureCollection",
+        features: filtered
+      });
+
+      map.fitBounds(bboxCoords, { linear: true, maxZoom: 17, maxDuration: 4000 });
+
+      document.querySelector(".featureDiv-active")?.classList.remove("featureDiv-active");
+      featureDiv.classList.add("featureDiv-active");
     });
 
-    map.fitBounds(bboxCoords, { linear: true, maxZoom: 17, maxDuration: 4000 });
-  });
+    const goodButton = featureDiv.querySelector("#good");
+    const faultButton = featureDiv.querySelector("#fault");
 
-  const goodButton = featureDiv.querySelector("#good");
-  const faultButton = featureDiv.querySelector("#fault");
+    const geojsonObject = readFromLocalStorage();
 
-  const geojsonObject = JSON.parse(localStorage.getItem("geojsonObject") || "{}");
-  console.log('\x1b[31m%s\x1b[0m', 'geojsonObject', geojsonObject);
+    if (geojsonObject[feature.properties.id] === "good") {
+      goodButton && makeActive(goodButton);
+    } else if (geojsonObject[feature.properties.id] === "fault") {
+      faultButton && makeActive(faultButton);
+    }
 
+    goodButton?.addEventListener("click", (event) => {
+      event.stopPropagation();
+      makeActive(goodButton);
+      faultButton && makeInactive(faultButton);
+      writeToLocalStorage(feature.properties.id, "good")
+    });
 
-  if (geojsonObject[feature.properties.id] === "good") {
-    goodButton && makeActive(goodButton);
-  } else if (geojsonObject[feature.properties.id] === "fault") {
-    faultButton && makeActive(faultButton);
-  }
+    faultButton?.addEventListener("click", (event) => {
+      event.stopPropagation();
+      makeActive(faultButton);
+      goodButton && makeInactive(goodButton);
+      writeToLocalStorage(feature.properties.id, "fault")
+    });
 
-  goodButton?.addEventListener("click", (event) => {
-    event.stopPropagation();
-    makeActive(goodButton);
-    faultButton && makeInactive(faultButton);
-    writeToLocalStorage(feature.properties.id, "good")
-  });
+    items?.appendChild(featureDiv);
+  };
+}
 
-  faultButton?.addEventListener("click", (event) => {
-    event.stopPropagation();
-    makeActive(faultButton);
-    goodButton && makeInactive(goodButton);
-    writeToLocalStorage(feature.properties.id, "fault")
-  });
+const init = () => {
+  const filtered = filterGeojson(f => true);
 
-  sidebar?.appendChild(featureDiv);
+  renderItemsList(filtered);
+  map.setFilter("markers", null);
 };
+
+init();
